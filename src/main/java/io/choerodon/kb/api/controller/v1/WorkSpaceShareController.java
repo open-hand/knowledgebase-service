abd67518ce128513dd52f970538d2aa7c4bbab2e
陈士男 <shinan.chen@hand-china.com>
2019-06-18 21:03:32 +0800

    [ADD] 新增分享链接的导出pdf和toc

--- src/main/java/io/choerodon/kb/api/controller/v1/WorkSpaceShareController.java
+++ src/main/java/io/choerodon/kb/api/controller/v1/WorkSpaceShareController.java
@@ -1,21 +1,20 @@
 package io.choerodon.kb.api.controller.v1;
 
-import java.util.List;
-
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import io.choerodon.base.annotation.Permission;
 import io.choerodon.kb.api.dao.PageAttachmentDTO;
 import io.choerodon.kb.api.dao.PageDTO;
 import io.choerodon.kb.api.dao.WorkSpaceFirstTreeDTO;
+import io.choerodon.kb.app.service.PageService;
 import io.choerodon.kb.app.service.WorkSpaceShareService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * Created by Zenger on 2019/6/11.
@@ -24,11 +23,10 @@ import io.choerodon.kb.app.service.WorkSpaceShareService;
 @RequestMapping(value = "/v1/work_space_share")
 public class WorkSpaceShareController {
 
+    @Autowired
     private WorkSpaceShareService workSpaceShareService;
-
-    public WorkSpaceShareController(WorkSpaceShareService workSpaceShareService) {
-        this.workSpaceShareService = workSpaceShareService;
-    }
+    @Autowired
+    private PageService pageService;
 
     @Permission(permissionPublic = true)
     @ApiOperation(value = "查询分享链接的树形结构")
@@ -62,5 +60,24 @@ public class WorkSpaceShareController {
             @RequestParam("token") String token) {
         return new ResponseEntity<>(workSpaceShareService.queryPageAttachment(workSpaceId, token),
                 HttpStatus.OK);
+    }
+
+    @Permission(permissionPublic = true)
+    @ApiOperation(value = "查询分享链接的文章标题")
+    @GetMapping(value = "/{id}/toc")
+    public ResponseEntity<String> pageToc(
+            @ApiParam(value = "页面ID", required = true)
+            @PathVariable Long id) {
+        return new ResponseEntity<>(pageService.pageToc(id), HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @Permission(permissionPublic = true)
+    @ApiOperation("导出文章为pdf")
+    @GetMapping(value = "/export_pdf")
+    public void exportMd2Pdf(@ApiParam(value = "页面id", required = true)
+                             @RequestParam Long pageId,
+                             HttpServletResponse response) {
+        workSpaceShareService.exportMd2Pdf(pageId, response);
     }
 }
