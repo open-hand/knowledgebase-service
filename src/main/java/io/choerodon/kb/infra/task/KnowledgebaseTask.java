bf9ac01e39448aecd6867affaa9969603afead67
陈士男 <shinan.chen@hand-china.com>
2019-06-26 19:24:46 +0800

    [FIX] syncWikiToNewKnowledgeBase增加设置当前线程用户

--- src/main/java/io/choerodon/kb/infra/task/KnowledgebaseTask.java
+++ src/main/java/io/choerodon/kb/infra/task/KnowledgebaseTask.java
@@ -7,6 +7,7 @@ import io.choerodon.asgard.schedule.QuartzDefinition;
 import io.choerodon.asgard.schedule.annotation.JobTask;
 import io.choerodon.asgard.schedule.annotation.TimedTask;
 import io.choerodon.core.iam.ResourceLevel;
+import io.choerodon.core.oauth.DetailsHelper;
 import io.choerodon.kb.app.service.WikiMigrationService;
 import io.choerodon.kb.infra.feign.AgileFeignClient;
 import org.slf4j.Logger;
@@ -33,6 +34,7 @@ public class KnowledgebaseTask {
     @JobTask(maxRetryCount = 3, code = "syncWikiToKnowledgebase", description = "升级到0.18.0,同步wiki数据到新知识管理")
     @TimedTask(name = "syncWikiToKnowledgebase", description = "升级到0.18.0,同步wiki数据到新知识管理", oneExecution = true, repeatCount = 0, repeatInterval = 1, repeatIntervalUnit = QuartzDefinition.SimpleRepeatIntervalUnit.HOURS, params = {})
     public void syncWikiToNewKnowledgeBase(Map<String, Object> map) {
+        DetailsHelper.setCustomUserDetails(1L,"zh_CN");
         LOGGER.info("==================================== Begin to upgrade 0.17.0 to 0.18.0, sync wiki to new knowledgebase! ====================================");
         wikiMigrationService.migration();
         LOGGER.info("==================================== Finished to sync wiki to new knowledgebase! =============================================");
