dce3fd03d6f65b3f5dc60973a27c2e0a97803cbe
Song <jian.song01@hand-china.com>
2019-08-23 18:09:53 +0800

    [FIX]修改翻译

--- react/locale/zh_CN.js
+++ react/locale/zh_CN.js
@@ -29,7 +29,7 @@ const zh_CN = {
   save: '保存',
 
   'page.doc.create': '创建文档',
-  'page.doc.buzz': '协作连接',
+  'page.doc.buzz': '协作共享',
   'doc.attachment': '附件',
   'doc.comment': '评论',
   'doc.comment.create': '创建评论',
