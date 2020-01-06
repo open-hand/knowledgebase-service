86ad4c740df572460fa9384e4c4b18205f32f1a9
lizhaozhong <zhaozhong.li@hand-china.com>
2020-01-06 18:07:50 +0800

    [IMP] 迁移数据接口实现

---
+++ src/main/java/io/choerodon/kb/api/controller/v1/DataMigrateController.java
@@ -0,0 +1,15 @@
+package io.choerodon.kb.api.controller.v1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: 25499
+ * @date: 2020/1/6 17:46
+ * @description:
+ */
+@RestController
+@RequestMapping("/v1/projects/{project_id}/fix")
+public class DataMigrateController {
+
+}
---
+++ src/main/java/io/choerodon/kb/app/service/impl/DataMigrateServiceImpl.java
@@ -0,0 +1,15 @@
+package io.choerodon.kb.app.service.impl;
+
+import io.choerodon.kb.app.service.DataMigrateService;
+
+/**
+ * @author: 25499
+ * @date: 2020/1/6 18:05
+ * @description:
+ */
+public class DataMigrateServiceImpl implements DataMigrateService {
+    @Override
+    public void migrateWorkSpace() {
+
+    }
+}
