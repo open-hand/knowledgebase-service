8356947908eda8ea811821e4859e7606c2ef98a2
lizhaozhong <zhaozhong.li@hand-china.com>
2020-01-06 18:51:16 +0800

    [IMP] 迁移数据接口实现

--- src/main/java/io/choerodon/kb/app/service/impl/DataMigrateServiceImpl.java
+++ src/main/java/io/choerodon/kb/app/service/impl/DataMigrateServiceImpl.java
@@ -1,6 +1,14 @@
 package io.choerodon.kb.app.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import io.choerodon.kb.app.service.DataMigrateService;
+import io.choerodon.kb.app.service.KnowledgeBaseService;
+import io.choerodon.kb.infra.dto.WorkSpaceDTO;
+import io.choerodon.kb.infra.feign.BaseFeignClient;
+import io.choerodon.kb.infra.mapper.WorkSpaceMapper;
 
 /**
  * @author: 25499
@@ -8,8 +16,19 @@ import io.choerodon.kb.app.service.DataMigrateService;
  * @description:
  */
 public class DataMigrateServiceImpl implements DataMigrateService {
+    @Autowired
+    private WorkSpaceMapper workSpaceMapper;
+    @Autowired
+    private KnowledgeBaseService knowledgeBaseService;
+    @Autowired
+    private BaseFeignClient baseFeignClient;
     @Override
     public void migrateWorkSpace() {
 
+        //1.修复workspace
+        List<WorkSpaceDTO> workSpaceDTOS = workSpaceMapper.selectAllOrganization();
+        //组织
+       
+        //项目
     }
 }
