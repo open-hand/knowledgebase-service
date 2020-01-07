f9e240551b458b860c4c8dd077f77430fe094c32
lizhaozhong <zhaozhong.li@hand-china.com>
2020-01-07 18:02:50 +0800

    [IMP] 优化代码

--- src/main/java/io/choerodon/kb/app/service/assembler/KnowledgeBaseAssembler.java
+++ src/main/java/io/choerodon/kb/app/service/assembler/KnowledgeBaseAssembler.java
@@ -55,15 +55,15 @@ public class KnowledgeBaseAssembler {
     }
 
     public void docheage(List<KnowledgeBaseListVO> knowledgeBaseListVOList, Long organizationId, Long projectId) {
-        if (CollectionUtils.isEmpty(knowledgeBaseListVOList)) {
+        List<Long> baseIds = knowledgeBaseListVOList.stream().map(KnowledgeBaseListVO::getId).collect(Collectors.toList());
+        if (CollectionUtils.isEmpty(baseIds)) {
             return;
         }
-        List<Long> baseIds = knowledgeBaseListVOList.stream().map(KnowledgeBaseListVO::getId).collect(Collectors.toList());
         List<WorkSpaceRecentVO> querylatestWorkSpace = workSpaceMapper.querylatest(organizationId, projectId, baseIds);
+        Map<Long, List<WorkSpaceRecentVO>> collect = querylatestWorkSpace.stream().collect(Collectors.groupingBy(WorkSpaceRecentVO::getBaseId));
         if (CollectionUtils.isEmpty(querylatestWorkSpace)) {
             return;
         }
-        Map<Long, List<WorkSpaceRecentVO>> baseMap = querylatestWorkSpace.stream().collect(Collectors.groupingBy(WorkSpaceRecentVO::getBaseId));
 
         //查询组织/项目名称
         List<Long> userIds = querylatestWorkSpace.stream().map(WorkSpaceRecentVO::getLastUpdatedBy).collect(Collectors.toList());
@@ -78,7 +78,7 @@ public class KnowledgeBaseAssembler {
                 Map<Long, String> map = projectDOS.stream().collect(Collectors.toMap(ProjectDO::getId, ProjectDO::getName));
                 baseListVO.setRangeName(map.get(baseListVO.getProjectId()));
             }
-            for (Map.Entry<Long, List<WorkSpaceRecentVO>> workMap : baseMap.entrySet()) {
+            for (Map.Entry<Long, List<WorkSpaceRecentVO>> workMap : collect.entrySet()) {
                 if (baseListVO.getId().equals(workMap.getKey())) {
                     List<WorkSpaceRecentVO> value = workMap.getValue();
                     value.stream().forEach(work->handleWorkSpace(work,userDOMap,organizationId,projectId));
