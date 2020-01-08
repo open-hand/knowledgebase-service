83e1636799711f4bebee18e99e19c541c4a85df7
812913 <lwfvirgo@163.com>
2020-01-08 20:45:20 +0800

    [FIX] 删除回收站的知识库添加弹框提示 & 恢复文档时判断其所属知识库是否被删除如果删除请选择新的知识库

--- react/api/knowledgebaseApi.js
+++ react/api/knowledgebaseApi.js
@@ -35,11 +35,16 @@ export const moveToBin = (id) => request.put(`/knowledge/v1/projects/${getProjec
 // 将组织下的知识库或文档移动到回收站
 export const orgMoveToBin = (id) => request.put(`/knowledge/v1/organizations/${getOrganizationId()}/knowledge_base/remove_my/${id}`);
 
+// 判断项目层文档所属知识库是否存在
+export const judgeBelongBaseIsExist = (docId) => request.get(`/knowledge/v1/projects/${getProjectId()}/work_space/belong_base_exist/${docId}`);
+// 判断组织层文档所属知识库是否存在
+export const judgeOrgBelongBaseIsExist = (docId) => request.get(`/knowledge/v1/organizations/${getOrganizationId()}/work_space/belong_base_exist/${docId}`);
+
 // 恢复回收站中的知识库或文档
-export const recoverFromBin = (id, type) => request.put(`/knowledge/v1/projects/${getProjectId()}/recycle/restore/${id}?type=${type}`);
+export const recoverFromBin = (id, type, baseId) => request.put(`/knowledge/v1/projects/${getProjectId()}/recycle/restore/${id}?type=${type}${baseId ? `&baseId=${baseId}` : ''}`);
 
 // 恢复组织层回收站中的知识库或文档
-export const recoverOrgFromBin = (id, type) => request.put(`/knowledge/v1/organizations/${getOrganizationId()}/recycle/restore/${id}?type=${type}`);
+export const recoverOrgFromBin = (id, type, baseId) => request.put(`/knowledge/v1/organizations/${getOrganizationId()}/recycle/restore/${id}?type=${type}${baseId ? `&baseId=${baseId}` : ''}`);
 
 // 删除回收站的知识库或文档
 export const deleteDocOrBase = (id, type) => request.delete(`/knowledge/v1/projects/${getProjectId()}/recycle/delete/${id}?type=${type}`);
--- react/routes/knowledge-bases/components/binTable/BinTable.js
+++ react/routes/knowledge-bases/components/binTable/BinTable.js
@@ -1,13 +1,25 @@
-import React, { useContext } from 'react';
-import { Table, TextField, Select } from 'choerodon-ui/pro';
+import React, { useContext, useMemo } from 'react';
+import { Table, TextField, Select, Modal, Form, DataSet } from 'choerodon-ui/pro';
 import { Action } from '@choerodon/boot';
 import { observer } from 'mobx-react-lite';
 import Store from '../../stores';
-import { deleteDocOrBase, deleteOrgDocOrBase, recoverFromBin, recoverOrgFromBin } from '../../../../api/knowledgebaseApi';
+import { deleteDocOrBase, deleteOrgDocOrBase, recoverFromBin, recoverOrgFromBin, judgeBelongBaseIsExist, judgeOrgBelongBaseIsExist } from '../../../../api/knowledgebaseApi';
 import UserHead from '../../../../components/UserHead';
+import RecoverDocModalDataSet from './RecoverDocModalDataSet';
 import './BinTable.less';
 
 const { Column } = Table;
+
+const RecoverDocModal = observer(({ recoverDocModalDataSet, record }) => {
+  return (
+    <div className="c7n-kb-recoverDocModal-children">
+      <span className="c7n-kb-recoverDocModal-children-tip">{`此文档所在的知识库“${record.get('belongToBaseName')}”已被删除，请选择此文档存放的知识库地址。`}</span>
+      <Form dataSet={recoverDocModalDataSet}>
+        <Select name="baseId" />
+      </Form>
+    </div>
+  );
+});
 
 const BinTable = observer(() => {
   const { binTableDataSet, knowledgeHomeStore, type } = useContext(Store);
@@ -15,7 +27,7 @@ const BinTable = observer(() => {
     if (record.get('type') !== 'page') {
       return '/';
     } else {
-      return '文档所属知识库名';
+      return record.get('belongToBaseName');
     }
   };
 
@@ -33,17 +45,71 @@ const BinTable = observer(() => {
     <UserHead user={record.get('lastUpdatedUser')} />
   );
 
+  const recoverProjectFromBin = ({ record, dataSet, baseId }) => {
+    recoverFromBin(record.get('id'), record.get('type'), baseId).then(() => {
+      dataSet.query();
+      knowledgeHomeStore.axiosProjectBaseList();
+    });
+  };
+
+  const recoverOrganizationFromBin = ({ record, dataSet, baseId }) => {
+    recoverOrgFromBin(record.get('id'), record.get('type'), baseId).then(() => {
+      dataSet.query();
+      knowledgeHomeStore.axiosOrgBaseList();
+    });
+  };
+
+  const handleRecoverDoc = async ({ recoverDocModalDataSet, record, dataSet }) => {
+    const data = recoverDocModalDataSet.toData()[0];
+    if (data && data.baseId) {
+      if (type === 'project') {
+        recoverProjectFromBin({ record, dataSet, baseId: data.baseId });
+      } else {
+        recoverOrganizationFromBin({ record, dataSet, baseId: data.baseId });
+      }
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const openSelectBaseModal = (record, dataSet) => {
+    const recoverDocModalDataSet = new DataSet(RecoverDocModalDataSet({ knowledgeHomeStore, type }));
+    Modal.open({
+      key: Modal.key(),
+      title: '恢复文档',
+      children: <RecoverDocModal recoverDocModalDataSet={recoverDocModalDataSet} record={record} recoverProjectFromBin={recoverProjectFromBin} recoverOrganizationFromBin={recoverOrganizationFromBin} />,
+      okText: '保存',
+      cancel: '取消',
+      onOk: () => handleRecoverDoc({ recoverDocModalDataSet, record, dataSet }),
+      style: { width: '5.6rem' },
+      className: 'c7n-kb-recoverDocModal',
+    });
+  };
+
   const handleRecoverFromBin = (record, dataSet) => {
     if (type === 'project') {
-      recoverFromBin(record.get('id'), record.get('type')).then(() => {
-        dataSet.query();
-        knowledgeHomeStore.axiosProjectBaseList();
+      if (record.get('type') === 'page') {
+        judgeBelongBaseIsExist(record.get('id')).then((isExist) => {
+          if (isExist === 'true') {
+            recoverProjectFromBin({ record, dataSet });
+          } else {
+            openSelectBaseModal(record, dataSet);
+          }
+        });
+      } else {
+        recoverProjectFromBin({ record, dataSet });
+      }
+    } else if (record.get('type') === 'page') {
+      judgeOrgBelongBaseIsExist(record.get('id')).then((isExist) => {
+        if (isExist === 'true') {
+          recoverOrganizationFromBin({ record, dataSet });
+        } else {
+          openSelectBaseModal(record, dataSet);
+        }
       });
     } else {
-      recoverOrgFromBin(record.get('id'), record.get('type')).then(() => {
-        dataSet.query();
-        knowledgeHomeStore.axiosOrgBaseList();
-      });
+      recoverOrganizationFromBin({ record, dataSet });
     }
   };
 
@@ -61,13 +127,27 @@ const BinTable = observer(() => {
     }
   };
 
+  const openDeletePromptModal = (record, dataSet) => {
+    let typeName = '知识库';
+    if (record.get('type') === 'page') {
+      typeName = '文档';
+    } else if (record.get('type') === 'template') {
+      typeName = '模板';
+    }
+    Modal.confirm({
+      title: '确认删除',
+      children: `确认从回收站删除${typeName}“${record.get('name')}”？`,
+      onOk: () => handleDeleteDocOrBin(record, dataSet),
+    });
+  };
+
   const renderAction = ({ record, dataSet }) => {
     const actionDatas = [{
       text: '恢复',
       action: () => { handleRecoverFromBin(record, dataSet); },
     }, {
       text: '删除',
-      action: () => handleDeleteDocOrBin(record, dataSet),
+      action: () => openDeletePromptModal(record, dataSet),
     }];
     return <Action data={actionDatas} />;
   };
