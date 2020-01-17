5e545df715f0a12c166e7315e8bb4b08f0f37559
lizhaozhong <zhaozhong.li@hand-china.com>
2020-01-17 17:30:28 +0800

    [IMP] 删除文档后删除agile的引用关系

---
+++ src/main/java/io/choerodon/kb/infra/feign/fallback/AgileFeignClientFallback.java
@@ -0,0 +1,24 @@
+package io.choerodon.kb.infra.feign.fallback;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import io.choerodon.core.exception.FeignException;
+import io.choerodon.kb.api.vo.ProjectDTO;
+import io.choerodon.kb.infra.feign.AgileFeignClient;
+
+/**
+ * @author: 25499
+ * @date: 2020/1/17 16:58
+ * @description:
+ */
+@Component
+public class AgileFeignClientFallback implements AgileFeignClient {
+    private static final String DELETE_ERROR = "error.agile-service.delete";
+    @Override
+    public ResponseEntity<List<ProjectDTO>> deleteByworkSpaceId(Long projectId, Long spaceId) {
+        throw new FeignException(DELETE_ERROR);
+    }
+}
