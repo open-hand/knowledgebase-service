71ff02c2ed903088179cd0040fbd96e1ccf1c6df
superlee <superleader8@gmail.com>
2020-06-04 13:47:08 +0800

    [FIX] 修复AgileFeignClient fallback配置错误的问题

--- src/main/java/io/choerodon/kb/infra/feign/AgileFeignClient.java
+++ src/main/java/io/choerodon/kb/infra/feign/AgileFeignClient.java
@@ -2,6 +2,7 @@ package io.choerodon.kb.infra.feign;
 
 import java.util.List;
 
+import io.choerodon.kb.infra.feign.fallback.AgileFeignClientFallback;
 import io.swagger.annotations.ApiParam;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
@@ -9,12 +10,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import io.choerodon.kb.api.vo.ProjectDTO;
-import io.choerodon.kb.infra.feign.fallback.BaseFeignClientFallback;
 
 /**
  * Created by 25499 on 20120/1/17.
  */
-@FeignClient(value = "agile-service", fallback = BaseFeignClientFallback.class)
+@FeignClient(value = "agile-service", fallback = AgileFeignClientFallback.class)
 public interface AgileFeignClient {
 
     @DeleteMapping(value = "/v1/projects/{project_id}/knowledge_relation/delete/{space_id}")
