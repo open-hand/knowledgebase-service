a01c7bff8d714e9f0f3bed4534947d49149480b1
jiaxu.cui <hand1234>
2020-06-18 10:38:28 +0800

    [FIX] 修复加密key

--- src/main/java/io/choerodon/kb/api/vo/RecycleVO.java
+++ src/main/java/io/choerodon/kb/api/vo/RecycleVO.java
@@ -14,7 +14,7 @@ import org.hzero.starter.keyencrypt.core.Encrypt;
  * @description:
  */
 public class RecycleVO {
-    @Encrypt(EncryptConstants.TN_KB_KNOWLEDGE_BASE)
+    @Encrypt(EncryptConstants.TN_KB_WORKSPACE)
     private Long id;
 
     private String name;
