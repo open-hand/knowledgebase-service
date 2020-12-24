5a7bdfaa38cf005454208fed4e955b161c38746d
laincarl <1047972853@qq.com>
2020-12-24 14:51:44 +0800

    [IMP]更改配置

--- react/config.js
+++ react/config.js
@@ -1,11 +1,4 @@
 const config = {
-  server: 'http://api.staging.saas.hand-china.com',
-  // server: 'http://10.211.102.55:8080',
-  master: './node_modules/@choerodon/master/lib/master.js',
-  projectType: 'choerodon',
-  buildType: 'single',
-  dashboard: {},
-  outward: '/knowledge/share',
   modules: [
     '.',
   ],
