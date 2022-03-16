8b536911625f320fcc4d6c2e1fc890da794bb557
superlee <kaiwen.li@hand-china.com>
2022-03-16 09:45:25 +0800

    [FIX] 修复服务启动失败的问题

--- src/main/java/io/choerodon/kb/app/service/impl/PageAttachmentServiceImpl.java
+++ src/main/java/io/choerodon/kb/app/service/impl/PageAttachmentServiceImpl.java
@@ -8,6 +8,7 @@ import io.choerodon.kb.app.service.PageAttachmentService;
 import io.choerodon.kb.infra.common.BaseStage;
 import io.choerodon.kb.infra.dto.PageAttachmentDTO;
 import io.choerodon.kb.infra.dto.PageDTO;
+import io.choerodon.kb.infra.mapper.PageAttachmentProMapper;
 import io.choerodon.kb.infra.utils.ExpandFileClient;
 import io.choerodon.kb.infra.mapper.PageAttachmentMapper;
 import io.choerodon.kb.infra.repository.PageAttachmentRepository;
@@ -47,6 +48,8 @@ public class PageAttachmentServiceImpl implements PageAttachmentService {
     private ModelMapper modelMapper;
     private PageAttachmentMapper pageAttachmentMapper;
     private ExpandFileClient expandFileClient;
+    @Autowired
+    private PageAttachmentProMapper pageAttachmentProMapper;
 
     public PageAttachmentServiceImpl(ExpandFileClient expandFileClient,
                                      PageRepository pageRepository,
@@ -176,7 +179,7 @@ public class PageAttachmentServiceImpl implements PageAttachmentService {
 
     @Override
     public PageAttachmentVO queryByFileName(Long organizationId, Long projectId, String fileName) {
-        List<PageAttachmentDTO> result = pageAttachmentMapper.queryByFileName(organizationId, projectId, fileName);
+        List<PageAttachmentDTO> result = pageAttachmentProMapper.queryByFileName(organizationId, projectId, fileName);
         if (!result.isEmpty()) {
             PageAttachmentDTO dto = result.get(0);
             dto.setUrl(filePathService.generateFullPath(BaseStage.BACKETNAME, dto.getUrl()));
