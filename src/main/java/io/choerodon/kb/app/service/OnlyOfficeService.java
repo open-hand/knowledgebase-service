fd34a21edf31d548bc6cfeb5267a9c6e40a2eaec
wangxiang <xiangwang04@hand-china.com>
2022-05-06 16:53:53 +0800

    [FIX] 回退代码

---
+++ src/main/java/io/choerodon/kb/app/service/OnlyOfficeService.java
@@ -0,0 +1,9 @@
+package io.choerodon.kb.app.service;
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * Created by wangxiang on 2022/5/6
+ */
+public interface OnlyOfficeService {
+    void saveFile(JSONObject obj);
+}
