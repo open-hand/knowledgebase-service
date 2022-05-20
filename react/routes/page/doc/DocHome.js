ba05705ecd64cb3f65d58638acf716bffb577433
isaac <273070111@qq.com>
2022-05-20 17:10:19 +0800

    [FIX]delete switch button

--- react/routes/page/doc/DocHome.js
+++ react/routes/page/doc/DocHome.js
@@ -217,12 +217,13 @@ function DocHome() {
           }
           handleDeleteDoc(pageStore.getSelectItem, 'admin', callback);
         }
-      }, {
-        name: '切换WPS/OnlyOffice',
-        handler: () => {
-          fileRef?.current?.changeMode();
-        }
       }]
+      // , {
+      //   name: '切换WPS/OnlyOffice',
+      //   handler: () => {
+      //     fileRef?.current?.changeMode();
+      //   }
+      // }]
     }, {
       icon: 'zoom_out_map',
       handler: () => {
