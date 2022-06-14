115400eec2d4be19b1e407cedd75313dd3b3c346
maox <xm1440606925@126.com>
2022-06-14 11:44:49 +0800

    [FIX]调整知识库上传限制问题

--- react/routes/page/doc/webUploader.js
+++ react/routes/page/doc/webUploader.js
@@ -57,7 +57,7 @@ class WebUploader {
       swf: `webuploader/dist/Uploader.swf`,
       server: this.uploadUrl,
       chunked: true,
-      chunkSize: 1 * 1000 * 1000,
+      chunkSize: 10 * 1024 * 1024,
       threads: 3,
       resize: false,
       headers: {
