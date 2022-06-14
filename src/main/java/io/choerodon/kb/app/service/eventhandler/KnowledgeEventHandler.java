2e75304b64092617f2d801b37c605e4f3ff936c8
wangxiang <xiangwang04@hand-china.com>
2022-06-14 21:20:14 +0800

    [FIX] 处理上传路径

--- src/main/java/io/choerodon/kb/app/service/eventhandler/KnowledgeEventHandler.java
+++ src/main/java/io/choerodon/kb/app/service/eventhandler/KnowledgeEventHandler.java
@@ -110,6 +110,10 @@ public class KnowledgeEventHandler {
             seq = 2)
     public String knowledgeUploadFile(String message) {
         PageCreateWithoutContentVO pageCreateWithoutContentVO = JSONObject.parseObject(message, PageCreateWithoutContentVO.class);
+        //处理file路径
+        if (pageCreateWithoutContentVO.getFilePath().startsWith("/")) {
+            pageCreateWithoutContentVO.setFilePath(pageCreateWithoutContentVO.getFilePath().substring(1, pageCreateWithoutContentVO.getFilePath().length()));
+        }
         upLoadFileServer(pageCreateWithoutContentVO.getOrganizationId(), pageCreateWithoutContentVO);
         //workSpaceId
         WorkSpaceDTO spaceDTO = workSpaceMapper.selectByPrimaryKey(pageCreateWithoutContentVO.getRefId());
