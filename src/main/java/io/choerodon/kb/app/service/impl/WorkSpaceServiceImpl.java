ce3ef3b4f550a7a31ecc8a529e1f05359df2330c
汪翔 <xiang.wang@zknow.com>
2022-08-05 11:03:53 +0800

    [FIX] file文件改名调整

--- src/main/java/io/choerodon/kb/app/service/impl/WorkSpaceServiceImpl.java
+++ src/main/java/io/choerodon/kb/app/service/impl/WorkSpaceServiceImpl.java
@@ -1636,14 +1636,12 @@ public class WorkSpaceServiceImpl implements WorkSpaceService {
         if (org.apache.commons.lang3.StringUtils.equalsIgnoreCase(spaceDTO.getType(), WorkSpaceType.FILE.getValue())) {
             String fileType = CommonUtil.getFileType(spaceDTO.getFileKey());
             spaceDTO.setName(newName + "." + fileType);
+            //同步修改page表
+            updatePageTitle(spaceDTO);
         } else {
             spaceDTO.setName(newName);
         }
-
         this.baseUpdate(spaceDTO);
-        //同步修改page表
-        updatePageTitle(spaceDTO);
-
     }
 
     public void updatePageTitle(WorkSpaceDTO spaceDTO) {
